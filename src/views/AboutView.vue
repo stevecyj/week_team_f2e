3ca9d13217a413a2bf2091f561350b1288fcbb5f
stevecyj <ycsteven.tsao@gmail.com>
2022-05-16 17:02:54 +0800

    chore. test component

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <CardTest />
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import CardTest from '@/components/CardTest.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    CardTest
+  }
+}
+</script>

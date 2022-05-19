ae21b0f4f6f7b06e5bf8ea2a46cec9e05e728ee0
stevecyj <ycsteven.tsao@gmail.com>
2022-05-19 13:35:44 +0800

    WIP. navbar

--- src/App.vue
+++ src/App.vue
@@ -1,65 +1,7 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-      <a
-        class="navbar-brand"
-        href="#"
-      >Navbar</a>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div
-        id="navbarNav"
-        class="collapse navbar-collapse"
-      >
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a
-              class="nav-link active"
-              aria-current="page"
-              href="#"
-            >Home</a>
-          </li>
-          <li class="nav-item">
-            <a
-              class="nav-link"
-              href="#"
-            >Features</a>
-          </li>
-          <li class="nav-item">
-            <a
-              class="nav-link"
-              href="#"
-            >Pricing</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled">Disabled</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link :to="{name:'newpage'}">新增頁面</router-link>
-  </nav>
-  <div class="container">
-    <router-view />
-  </div>
-  <button
-    type="button"
-    class="btn btn-primary"
-  >Primary</button>
-  {{name}}
+  <!-- <div class="container-fluid">
+  </div> -->
+  <router-view />
 </template>
 
 <script>
---
+++ src/views/front/FrontView.vue
@@ -0,0 +1,46 @@
+<template lang="html">
+  <section>
+      <nav
+        class="navbar navbar-expand-lg navbar-light border-bottom border-3 border-dark"
+      >
+        <div class="container px-lg-12">
+          <h1 class="font-paytone">
+            <a class="navbar-brand fs-1" href="#">MetaWall</a>
+          </h1>
+          <div class="d-flex align-items-center">
+            <img src="@/assets/img/user1.png" alt="user1" class="me-2 img-fluid" />
+            <div class="dropdown bg-white me-2">
+              <button
+                id="dropdownMenuButton1"
+                class="btn btn-white border-0 text-decoration-underline fw-bolder font-azeret"
+                type="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Member
+              </button>
+              <ul
+                class="dropdown-menu py-0"
+                aria-labelledby="dropdownMenuButton1"
+              >
+                <li class="border border-dark">
+                  <a class="dropdown-item" href="#">我的貼文牆</a>
+                </li>
+                <li class="border border-dark">
+                  <a class="dropdown-item" href="#">修改個人資料</a>
+                </li>
+                <li class="border border-dark">
+                  <a class="dropdown-item" href="#">登出</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </section>
+</template>
+<script>
+export default {}
+</script>
+<style lang="">
+</style>
